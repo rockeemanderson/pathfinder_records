b7ef190d26d79d0daf95ca9119b3ac2fe541676a
rockeemanderson <56086450+rockeemanderson@users.noreply.github.com>
2019-12-14 22:22:29 -0500

    Update resources.php

--- resources.php
+++ resources.php
@@ -7,6 +7,12 @@ require_once 'db/conn.php';
 
  
 <?php
+
+$myfile = fopen("Adventurer/adv_adm_manual.pdf", "r") or die("Unable to open file!");
+echo fgets($myfile);
+fclose($myfile);
+
+
 $directory = "resources/Adventurer";
 
 if (is_dir($directory)){
